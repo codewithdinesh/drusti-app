f8c392a9b93af1a9e8ccbbcb99a1f5f4bc70f54d
Dinesh Rathod <pr760677@gmail.com>
2022-01-09 15:43:11 +0530

    bottom navigation

---
+++ app/src/main/java/com/drustii/MainActivity.java
@@ -0,0 +1,26 @@
+package com.drustii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+//MenuItem home,explore,favorite,profile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    private void setSupportActionBar(Toolbar myToolbar) {
+    }
+
+}
